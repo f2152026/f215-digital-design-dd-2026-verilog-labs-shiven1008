f722d1a693f064d6a4aee0847de9f0a929c38465
ShIven1008 <f20250695@pilani.bits-pilani.ac.in>
2026-09-19 14:48:42 +0000

    lab02 task4

--- labs/lab02/task4/and_beh_intra.v
+++ labs/lab02/task4/and_beh_intra.v
@@ -1,5 +1,5 @@
 module and_beh_intra (input wire a, input wire b, output reg y);
     always @(*) begin
-        y = #DELAY a & b; // Evaluates (a & b) immediately, delays updating y
+        y = #5 a & b; // Evaluates (a & b) immediately, delays updating y
     end
 endmodule
--- labs/lab02/task4/and_df.v
+++ labs/lab02/task4/and_df.v
@@ -1,3 +1,3 @@
 module and_df (input wire a, input wire b, output wire y);
-    assign #DELAY y = a & b; // Replace #DELAY with #1, #2, or #3
+    assign #5 y = a & b; // Replace #DELAY with #1, #2, or #3
 endmodule
